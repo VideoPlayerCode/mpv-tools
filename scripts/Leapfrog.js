5638ba05d9a4d09d6a821d1c90f103750771198a
SteveJobzniak <SteveJobzniak@users.noreply.github.com>
2017-12-19 18:53:14 +0100

    Leapfrog: Add "undo-random", tweak throttle & errors
    - Every time you use the "random" feature, it now saves the current
      position to a history stack which stores up to 200 entries.
    
    - Added "undo-random" which takes you back to the previous history stack
      entry. This lets you jump backwards to previous locations. Very useful
      when you blaze through the playlist with "random" and accidentally
      skip too far and want to return to one of the previous random locations.
    
    - Anytime you change the playlist, the history stack is cleared. It
      would be way too much work (for too little gain) to attempt to
      re-calculate all history-offsets to match the new playlist.
    
    - Throttling adjusted from 0.2s to 0.25s for slightly less insane speed.
      This may be tweaked again the future. It's hard to balance it to a
      speed that isn't too fast while holding down a key, but also not too
      slow when manually pressing the key repeatedly.
    
    - Errors can now be specifically silenced via "silenterr" without
      silencing regular jump-messages.
    
    - All commands now have a custom OSD prefix, such as "Random:" or
      "Undo:" or "Jump:", to shorten the text (it used to say "Jump: random").

--- scripts/Leapfrog.js
+++ scripts/Leapfrog.js
@@ -5,7 +5,7 @@
  *              jump size and direction, including the ability to jump randomly.
  *              Excellent when queuing lots of images and using mpv as an image
  *              viewer.
- * Version:     1.2.0
+ * Version:     1.3.0
  * Author:      SteveJobzniak
  * URL:         https://github.com/SteveJobzniak/mpv-tools
  * License:     Apache License, Version 2.0
@@ -18,8 +18,11 @@
 
 'use strict';
 
+var Stack = require('Stack');
+
 (function() {
     var throttleTime = 0,
+        history = new Stack(200),
         playlistPos = mp.get_property_number('playlist-pos'),
         playlistCount = mp.get_property_number('playlist-count');
     mp.observe_property('playlist-pos', 'number', function(name, value) {
@@ -27,18 +30,26 @@
     });
     mp.observe_property('playlist-count', 'number', function(name, value) {
         playlistCount = value;
+        // New count means new/changed playlist. Clear unreliable history.
+        // NOTE: When people queue folders, count changes to "1" (the folder)
+        // and then the real file count, so re-queuing same/diff folders with
+        // same amount of files is accurately detected and cleared too.
+        history.clearStack();
     });
 
     // Provide the bindable mpv command which performs the playlist jump.
     // * Bind this via input.conf: `ctrl+x script-message Leapfrog -10`.
     // - Jumps can be either positive (ie. `100`) or negative (ie. `-3`).
     // - Use the word `random` to perform completely random jumps, as follows:
-    //   `ctrl+x script-message Leapfrog random`.
+    //   `script-message Leapfrog random`.
+    // - To undo your random jumps, use `script-message Leapfrog undo-random`.
     // - You can silence the on-screen message by adding the option "silent"
-    //   at the end: `ctrl+x script-message Leapfrog 5 silent`.
+    //   at the end: `script-message Leapfrog 5 silent`.
+    // - To only silence error messages, use "silenterr" instead (this is useful
+    //   together with "undo-random" to prevent the error msg when no history).
     // - If you want to be able to hold down the key, you should bind it with
     //   the "repeatable" flag and the "throttle" option, as follows:
-    //   `ctrl+x repeatable script-message Leapfrog 1 throttle`. The throttling
+    //   `repeatable script-message Leapfrog 1 throttle`. The throttling
     //   ensures playlist progression at a sane pace when the key is held down.
     // - Lastly, you can combine multiple options by separating them with
     //   commas, such as: `Leapfrog random throttle,silent`.
@@ -57,27 +68,43 @@
         // Handle throttling.
         if (options.throttle) {
             var now = mp.get_time_ms();
-            if (now - throttleTime < 200) // 0.2s
+            if (now - throttleTime < 250) // 0.25s
                 return;
             throttleTime = now;
         }
 
         // Calculate new playlist position.
-        var newPosition;
+        var saveOldPosition = false,
+            newPosition, msgPrefix;
         switch (offset) {
+        case 'undo-random':
+            var previousPosition = history.pop();
+            if (typeof previousPosition !== 'number') {
+                if (!options.silent && !options.silenterr)
+                    mp.osd_message('Undo: No history.');
+                return;
+            }
+            newPosition = previousPosition;
+            msgPrefix = 'Undo:';
+            break;
         case 'random':
+            saveOldPosition = true;
             do {
                 newPosition = Math.floor(Math.random() * playlistCount);
             } while(newPosition === playlistPos && playlistCount > 1);
+            msgPrefix = 'Random:';
             break;
         default:
             offset = parseInt(offset, 10);
             if (isNaN(offset) || offset === 0) {
                 mp.msg.error('Leapfrog: Invalid offset number.');
+                if (!options.silent && !options.silenterr)
+                    mp.osd_message('Jump: Invalid offset number.');
                 return;
             }
 
             newPosition = playlistPos + offset;
+            msgPrefix = 'Jump: '+(offset > 0 ? '+' : '')+offset;
         }
 
         // Clamp position value to edges of playlist.
@@ -86,14 +113,17 @@
         else if (newPosition >= playlistCount)
             newPosition = playlistCount - 1;
 
+        // Save old position to history, but only if different.
+        if (saveOldPosition && newPosition !== playlistPos)
+            history.push(playlistPos);
+
         // Update position.
         mp.set_property('playlist-pos', newPosition);
 
         // Display on-screen feedback.
         if (!options.silent)
             mp.osd_message(
-                'Jump: '+(offset !== 'random' && offset > 0 ? '+' : '')+offset+
-                    ' ('+(newPosition + 1)+' / '+playlistCount+')',
+                msgPrefix+' ('+(newPosition + 1)+' / '+playlistCount+')',
                 1.5
             );
     });
